b807d5c4deb439abe8dc4d3fc739e1f7fec66bcd
Ridvan2002 <ridvansuleymanov28@gmail.com>
2024-11-09 14:02:17 -0500

    pictures fix

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,10 @@ import React from "react";
 function Home() {
     return (
         <div className="home-container">
-            <h1 className="name-title">Hello! I'm Ridvan Suleymanov</h1>
+            <h1 className="name-title">
+                <span className="greeting">Hello!</span><br />
+                <span className="name">I'm Ridvan Suleymanov</span>
+            </h1>
             <h2 className="title-subtitle">UX/UI Designer, Web Developer</h2>
             <p className="intro-text">"Good judgment comes from experience, and a lot of that comes from bad judgment." - Will Rogers</p>
         </div>
